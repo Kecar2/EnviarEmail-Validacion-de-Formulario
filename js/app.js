c1c739dcdb1dc1606b34cf767d01e10db59d9276
Kecar2 <kendrycarvajalsegura@gmail.com>
2022-03-21 11:41:25 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,81 @@
+// Variables 
+let btnEnviar = document.querySelector('#enviar');
+let formulario = document.querySelector('#enviar-mail');
+
+// variables para campos
+let email = document.querySelector('#email');
+let asunto = document.querySelector('#asunto');
+let mensaje = document.querySelector('#mensaje');
+let resetBtn = document.querySelector('#resetBtn');
+
+let er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+// event Listener
+
+eventListeners();
+
+function eventListeners() {
+     // Inicio de la aplicación y deshabilitar submit
+     document.addEventListener('DOMContentLoaded', inicioApp);
+
+     // Campos del formulario
+     email.addEventListener('blur', validarFormulario);
+     asunto.addEventListener('blur', validarFormulario);
+     mensaje.addEventListener('blur', validarFormulario);
+
+     // Boton de enviar en el submit
+     formulario.addEventListener('submit', enviarEmail);
+
+     // Boton de reset
+     resetBtn.addEventListener('click', resetFormulario);
+}
+
+
+
+// funciones
+function inicioApp() {
+     // deshabilitar el envio
+     btnEnviar.disabled = true;
+     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50')
+}
+
+
+// validar el formulario
+
+function validarFormulario(e) {
+
+    if(e.target.value.length > 0 ) {
+
+        // Elimina los errores...
+        let error = document.querySelector('p.error');
+        if(error) {
+            error.remove();
+        }
+        e.target.classList.remove('border', 'border-red-500');
+        e.target.classList.add('border', 'border-green-500');
+
+    }else {
+        e.target.classList.remove('border', 'border-green-500');
+        e.target.classList.add('border', 'border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if(e.target.type === 'email') {
+        if(er.test( e.target.value )){
+            // Elimina los errores
+            let error = document.querySelector('p.error');
+            if(error) {
+                error.remove();
+            }
+            e.target.classList.remove('border', 'border-red-500');
+            e.target.classList.add('border', 'border-green-500');
+        } else {
+            e.target.classList.remove('border', 'border-green-500');
+            e.target.classList.add('border', 'border-red-500');
+        }
+    }
+}
+
+
+    
